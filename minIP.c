88b9f7bf5dc4edcb88f64a4b1a5f4a2ed7b365f1
Ian Seyler <ian.seyler@returninfinity.com>
2015-02-28 16:03:44 -0500

    Some TCP handling

--- minIP.c
+++ minIP.c
@@ -103,31 +103,28 @@ int main(int argc, char *argv[])
 		retval = net_recv(buffer);
 		if (retval > 0)
 		{
-			if (ntohs(*(unsigned short *) &buffer[12]) == 0x0806)
+			if (ntohs(*(unsigned short *) &buffer[12]) == 0x0806) // ARP
 			{
-				printf("\nARP - ");
+			//	printf("\nARP - ");
 				if (buffer[21] == 0x01)
 				{
-					printf("Request - Who is %d.%d.%d.%d? Tell %d.%d.%d.%d", buffer[38], buffer[39], buffer[40], buffer[41], buffer[28], buffer[29], buffer[30], buffer[31]);
+			//		printf("Request - Who is %d.%d.%d.%d? Tell %d.%d.%d.%d", buffer[38], buffer[39], buffer[40], buffer[41], buffer[28], buffer[29], buffer[30], buffer[31]);
 					if ((buffer[38] == src_IP[0]) & (buffer[39] == src_IP[1]) & (buffer[40] == src_IP[2]) & (buffer[41] == src_IP[3]))
 					{
-						printf(" - Looking for me?");
+			//			printf(" - Looking for me?");
 						memcpy((void*)tosend, (void*)buffer, ETH_FRAME_LEN); // make a copy of the original frame
-						memcpy((void*)tosend, (void*)tosend+6, 6); // copy the incoming MAC to destination
+						memcpy((void*)tosend, (void*)buffer+6, 6); // copy the incoming MAC to destination
 						memcpy((void*)tosend+6, (void*)src_MAC, 6); // copy the source MAC
-						memcpy((void*)tosend+32, (void*)tosend+22, 10); // copy sender+mac ID to target
-						tosend[21] = 0x02; // reply
+						memcpy((void*)tosend+32, (void*)buffer+22, 10); // copy sender+mac ID to target
+						tosend[21] = 0x02; // ARP reply
 						memcpy((void*)tosend+22, (void*)src_MAC, 6); // copy the source MAC
-						tosend[28] = src_IP[0];
-						tosend[29] = src_IP[1];
-						tosend[30] = src_IP[2];
-						tosend[31] = src_IP[3];
+						memcpy((void*)tosend+28, (void*)buffer+38, 4); // copy IP address
 						net_send(tosend, 42);
 					}
 				}
 				else if (buffer[21] == 0x02)
 				{
-					printf("Response");
+			//		printf("Response");
 				}
 			}
 			else if (ntohs(*(unsigned short *) &buffer[12]) == 0x0800) // buffer[12] == 0x08 & buffer[13] == 0x00
@@ -144,18 +141,15 @@ int main(int argc, char *argv[])
 							// Reply to the ping request
 							printf(" - Replying!");
 							memcpy((void*)tosend, (void*)buffer, ETH_FRAME_LEN); // make a copy of the original frame
-							memcpy((void*)tosend, (void*)tosend+6, 6); // copy the incoming MAC to destination
-							memcpy((void*)tosend+6, (void*)src_MAC, 6); // copy the source MAC
-							memcpy((void*)tosend+30, (void*)tosend+26, 4); // copy the incoming IP to destination
-							memcpy((void*)&tshort, (void*)tosend+16, 2); // copy the packet length
-							tosend[26] = src_IP[0];
-							tosend[27] = src_IP[1];
-							tosend[28] = src_IP[2];
-							tosend[29] = src_IP[3];
-							tosend[34] = 0x00; // reply
-							tosend[36] = 0x00; // clear checksum
-							tosend[37] = 0x00; // clear checksum
-							checksumval = checksum(&tosend[34], ntohs(tshort)-20);
+							memcpy((void*)tosend, (void*)buffer+6, 6); // copy the incoming MAC to destination
+							memcpy((void*)tosend+6, (void*)buffer, 6); // copy the source MAC
+							memcpy((void*)tosend+30, (void*)buffer+26, 4); // copy the incoming IP to destination
+							memcpy((void*)tosend+26, (void*)buffer+30, 4); // copy the destination IP to source
+							// No IP header checksum calc since the header contents were only shifted around
+							tosend[34] = 0x00; // ICMP type = reply
+							tosend[36] = 0x00; // clear ICMP checksum
+							tosend[37] = 0x00; // clear ICMP checksum
+							checksumval = checksum(&tosend[34], retval-14-20); // Frame length - MAC header - IP header
 							memcpy((void*)tosend+36, (void*)&checksumval, 2);
 							net_send(tosend, retval); // send the response
 						}
@@ -172,6 +166,37 @@ int main(int argc, char *argv[])
 				else if(buffer[23] == 0x06)
 				{
 					printf("TCP");
+					if ((buffer[47] & 0x02) == 0x02)
+					{
+						printf(" - SYN");
+						memcpy((void*)tosend, (void*)buffer, ETH_FRAME_LEN); // make a copy of the original frame
+						memcpy((void*)tosend, (void*)buffer+6, 6); // copy the incoming MAC to destination
+						memcpy((void*)tosend+6, (void*)buffer, 6); // copy the source MAC
+						memcpy((void*)tosend+30, (void*)buffer+26, 4); // copy the incoming IP to destination
+						memcpy((void*)tosend+26, (void*)buffer+30, 4); // copy the destination IP to source
+						memcpy((void*)&tshort, (void*)buffer+16, 2); // extract the packet length
+						memcpy((void*)tosend+34, (void*)buffer+36, 2); // copy destination port to source
+						memcpy((void*)tosend+36, (void*)buffer+34, 2); // copy source port to destination
+						memcpy((void*)&tint, (void*)buffer+38, 4); // extract sequence number
+						tint = ntohl(tint);
+						tint++; // increment sequence number
+						tint = htonl(tint);
+						memcpy((void*)tosend+42, (void*)&tint, 4); // store updated sequence number as acknowledgement number
+						tosend[47] |= 0x10; // set TCP ACK
+						tosend[50] = 0x00; // clear TCP checksum
+						tosend[51] = 0x00; // clear TCP checksum
+						tosend[retval] = 0x00;
+						tosend[retval+1] = 16;
+						tosend[retval+2] = 0;
+						tosend[retval+3] = 34;
+						checksumval = checksum(&tosend[26], retval-26+4); // Start checksum at Source IP so we only need to build the tail of the pseudo header
+						memcpy((void*)tosend+50, (void*)&checksumval, 2);
+						net_send(tosend, retval);
+					}
+					else if ((buffer[47] & 0x10) == 0x10)
+					{
+						printf(" - ACK");
+					}
 				}
 				else if (buffer[23] == 0x11)
 				{

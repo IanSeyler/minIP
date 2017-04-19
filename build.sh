934c4373de0ec642aec412b0370bc2768b7dfab7
Ian Seyler <ian@seyler.me>
2017-04-19 13:16:29 -0400

    Update build script
    Removes compile warnings about builtins

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-gcc -c -m64 -Wall -W -pedantic -std=c99 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o minIP.o minIP.c -DBAREMETAL_STANDALONE
-gcc -c -m64 -Wall -W -pedantic -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o libBareMetal.o libBareMetal.c
+gcc -c -m64 -Wall -W -pedantic -std=c99 -fno-builtin -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o minIP.o minIP.c -DBAREMETAL_STANDALONE
+gcc -c -m64 -Wall -W -pedantic -fno-builtin -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o libBareMetal.o libBareMetal.c
 objcopy --remove-section .comment minIP.o
 objcopy --remove-section .eh_frame minIP.o
 objcopy --remove-section .comment libBareMetal.o
